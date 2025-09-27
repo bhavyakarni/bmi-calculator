0260d4e2bec6627a0536b555c70d154b347947b1
bhavyakarni <153359930+bhavyakarni@users.noreply.github.com>
2025-09-27 11:36:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function calculateBMI() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+
+  if (weight === "" || height === "") {
+    document.getElementById("result").innerText = "⚠️ Please enter both values!";
+    return;
+  }
+
+  // convert height from cm to m
+  height = height / 100;
+
+  let bmi = (weight / (height * height)).toFixed(2);
+  let category = "";
+
+  if (bmi < 18.5) {
+    category = "Underweight";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    category = "Normal weight";
+  } else if (bmi >= 25 && bmi < 29.9) {
+    category = "Overweight";
+  } else {
+    category = "Obese";
+  }
+
+  document.getElementById("result").innerText = `Your BMI is ${bmi} (${category})`;
+}
